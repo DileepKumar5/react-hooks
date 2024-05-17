4e273556c2b7dbfa5d5c2008bbb9460b26b23445
DileepKumar5 <dileepkumar5062002@gmail.com>
2024-05-17 12:02:53 +0500

    react-hooks

--- components/ComponentC.tsx
+++ components/ComponentC.tsx
@@ -8,11 +8,11 @@ type  ComponentType = {
 
 const ComponentC = ({counter, setcounter}: ComponentType) => {
   return (
-    <div className='border border-black bg-orange-300 h-[75%] w-[75%] rounded-lg items-center justify-center flex flex-col'>
+    <div className='border border-black bg-orange-300 h-[75%] w-[75%] rounded-lg items-center justify-center flex flex-col gap-3'>
         ComponentC
-        <div>
+        <div className='flex gap-3 items-center justify-center'>
           <Button variant='destructive' onClick={ () => setcounter (prev => prev-1)}> - </Button>
-          {counter}
+          <span className='mx-3'>{counter}</span>
           <Button onClick={ () => setcounter (prev => prev+1)}> + </Button>
         </div>
       
